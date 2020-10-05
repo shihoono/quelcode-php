8f5df8844dc389d6f592e2e0c72d50f400cb60ba
shihoono <shiho1449@gmail.com>
2020-10-05 19:10:45 +0900

    add for statement program

--- html/php-pre-challenge1/index.php
+++ html/php-pre-challenge1/index.php
@@ -1,5 +1,14 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     //ここからコードを書く
-    echo $i;
+    if ($i % 3 == 0 && $i % 5 == 0){
+        echo nl2br("3の倍数であり、5の倍数\n");
+    } else if ($i % 3 == 0){
+        echo nl2br("3の倍数\n");
+    } else if ($i % 5 == 0){
+        echo nl2br("5の倍数\n");
+    } else {
+        echo $i.nl2br("\n");
+    }
 }
+?>
