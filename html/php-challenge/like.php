82e1f6796fcdad7ba1465e3ecc60adcb4679abb8
shihoono <shiho1449@gmail.com>
2020-10-10 10:24:07 +0900

    add function that remove likes from retweeted msg

--- html/php-challenge/like.php
+++ html/php-challenge/like.php
@@ -12,24 +12,65 @@ if (isset($_SESSION['id'])) {
     $_SESSION['id']  //like_member_id
   ));
   $like = $likes->fetch();
+  
+
+  $posts = $db->prepare('SELECT retweeted_post_id FROM posts WHERE id=?');
+    $posts->execute(array(
+        $_REQUEST['id']
+    ));
+    $post = $posts->fetch();
 
   // いいねしようとしているメッセージを、すでにいいねをしていないかチェック
   if ($like['liked_cnt'] == 0) {
     // いいねを登録する
-    $doLike = $db->prepare('INSERT INTO likes SET liked_post_id=?, like_member_id=?, created=NOW()');
+    $likes = $db->prepare('SELECT l.retweeted_post From likes l WHERE l.like_member_id=?');
+    $likes->execute(array(
+        $_SESSION['id']
+    ));
+    $like = $likes->fetch();
+
+    if ($post['retweeted_post_id'] > 0){
+        $doLike = $db->prepare('INSERT INTO likes SET liked_post_id=?, like_member_id=?, retweeted_post=?, created=NOW()');
+        $doLike->execute(array(
+            $post['retweeted_post_id'], 
+            $_SESSION['id'],
+            1 
+        ));
+    } else {
+        $doLike = $db->prepare('INSERT INTO likes SET liked_post_id=?, like_member_id=?, created=NOW()');
+        $doLike->execute(array(
+            $_REQUEST['id'], 
+            $_SESSION['id']  
+        ));
+    }   
+  }
+
+  // いいね取り消し
+
+  //リツートされたメッセージのリツート元のいいねの取り消し
+  if($like['retweeted_post'] == 1){
+    $doLike = $db->prepare('DELETE FROM likes WHERE liked_post_id=? AND like_member_id=?');
     $doLike->execute(array(
-      $_REQUEST['id'], 
-      $_SESSION['id']  
+        $post['retweeted_post_id'], 
+        $_SESSION['id']  
     ));
   }
 
+  //通常のメッセージのいいね取り消し
   if ($like['liked_cnt'] == 1) {
-    // いいねを削除する
-    $doLike = $db->prepare('DELETE FROM likes WHERE liked_post_id=? AND like_member_id=?');
-    $doLike->execute(array(
-      $_REQUEST['id'], 
-      $_SESSION['id']  
-    ));
+    if ($post['retweeted_post_id'] > 0){
+        $doLike = $db->prepare('DELETE FROM likes WHERE liked_post_id=? AND like_member_id=?');
+        $doLike->execute(array(
+            $post['retweeted_post_id'], 
+            $_SESSION['id']  
+        ));
+    } else {
+        $doLike = $db->prepare('DELETE FROM likes WHERE liked_post_id=? AND like_member_id=?');
+        $doLike->execute(array(
+            $_REQUEST['id'], 
+            $_SESSION['id']  
+        ));
+    }   
   }
 }
 
